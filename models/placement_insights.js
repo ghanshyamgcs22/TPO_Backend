4b43894b8efc2a5052d4ff677371045367c4875a
Ghanshyam Gupta <ghanshyamg.cs.22@nitj.ac.in>
2024-08-27 14:02:56 +0530

    first commit

---
+++ models/placement_insights.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+//----------------------------------->
+
+//Schema---------------------------->
+const Schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        course: {
+            type: String,
+            required: true,
+        },
+        department: {
+            type: String,
+            required: true,
+        },
+        package: {
+            type: Number,
+            required: true,
+        },
+        year: {
+            type: Number,
+            required: true,
+        },
+        company: {
+            type: String,
+            required: true,
+        },
+        image:{
+            type:String,
+            required:true
+        },
+        show: {
+            type: Boolean,
+            default: true
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+//Model---------------------------->
+const Model = mongoose.model("placement_insights", Schema);
+
+//Export----------------------------->
+module.exports = Model;
+
